01b0f389b81de0db0d5fcc7bf457e594aa8878c0
Kazalisto <keath.kaza@gmail.com>
2021-12-17 23:54:58 +0000

    looping thru arrays

---
+++ main.js
@@ -0,0 +1,41 @@
+const todos = [
+{
+    id: 1,
+    text: "Take out the trash",
+    isCompleted : false,
+    age: 45
+},
+{
+    id: 2,
+    text: "Meeting with the Boss",
+    isCompleted : true,
+    age: 65,
+},
+{
+    id: 3,
+    text: "Dentist app",
+    isCompleted : false,
+    age: 55
+},
+]
+
+//forEach, map, filter
+
+//1. forEach
+//todos.forEach(function(todo) {
+//    console.log(todo.text)
+//})
+
+//2. Map
+//const todoText = todos.map(function(todo){
+//    return todo.text
+//})
+//console.log(todoText)
+
+//3. Filter
+//const bigAge = todos.filter(function(age){
+//    return age.age < 50
+//})
+
+//console.log(bigAge)
+
