50e95e74849da2826b0d8b37511ac24ba64b3a34
Kazalisto <keath.kaza@gmail.com>
2021-12-20 00:16:22 +0000

    constructor function

--- main.js
+++ main.js
@@ -1,41 +1,11 @@
-const todos = [
-{
-    id: 1,
-    text: "Take out the trash",
-    isCompleted : false,
-    age: 45
-},
-{
-    id: 2,
-    text: "Meeting with the Boss",
-    isCompleted : true,
-    age: 65,
-},
-{
-    id: 3,
-    text: "Dentist app",
-    isCompleted : false,
-    age: 55
-},
-]
+// Constructor function 
+function Person(firstName, lastName, dob) {
+    this.firstName = firstName,
+    this.lastName = lastName,
+    this.dob = dob
+}
 
-//forEach, map, filter
+// Instatiate object
+const person1 = new Person ('John', 'Doe', '11/09/1993')
 
-//1. forEach
-//todos.forEach(function(todo) {
-//    console.log(todo.text)
-//})
-
-//2. Map
-//const todoText = todos.map(function(todo){
-//    return todo.text
-//})
-//console.log(todoText)
-
-//3. Filter
-//const bigAge = todos.filter(function(age){
-//    return age.age < 50
-//})
-
-//console.log(bigAge)
-
+console.log(person1)
